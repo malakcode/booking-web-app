364dcd7f86451f1123c58b4484e0d97574d50413
malakcode <malakmouhachim0@gmail.com>
2024-05-14 16:00:54 +0100

    first commit

---
+++ app/SignUp/page.js
@@ -0,0 +1,173 @@
+"use client";
+import React, { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import axios from 'axios';
+//import { useNavigate } from 'react-router-dom'; // or from '@reach/router' if you're using Reach Router
+
+export default function SignUp() {
+  const [formData, setFormData] = useState({
+    
+    username: '',
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: ''
+  });
+
+ //const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:8080/user/add', formData);
+      console.log(response.data);
+      // navigate('/SignIn');
+    } catch (error) {
+      console.error('Error signing up:', error);
+    }
+  };
+
+  return (
+    <div className="bg-white">
+      <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
+        <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
+          <img
+            alt=""
+            src="/signup.jpeg"
+            className="absolute inset-0 h-full w-full object-cover opacity-80"
+          />
+
+          <div className="hidden lg:relative lg:block lg:p-12">
+            <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
+              Sign up
+            </h2>
+
+            <p className="mt-4 leading-relaxed text-white/90">
+              create your first account and experience a new healthcare vision
+            </p>
+          </div>
+        </section>
+
+        <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
+          <div className="max-w-xl lg:max-w-3xl">
+            <div className="relative -mt-16 block lg:hidden">
+              <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
+                Sign Up
+              </h1>
+            </div>
+
+            <form action="#" className="mt-8 grid grid-cols-6 gap-6" onSubmit={handleSubmit}>
+              
+            <div className="col-span-6">
+                 <label htmlFor="username" className="block text-sm font-medium text-gray-700"> 
+                 Username
+                  </label>
+     
+                 <input
+                 onChange={handleChange} required
+                   type="text"
+                   name="username"
+                   value={formData.username}
+                   className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
+                 />
+               </div>
+
+
+                 <div className="col-span-6 sm:col-span-3">
+                 <label htmlFor="firstname" className="block text-sm font-medium text-gray-700">
+                   First Name
+                 </label>
+     
+                 <input
+                   onChange={handleChange} required
+                   type="text"
+                   name="firstname"
+                   value={formData.firstname}
+                   className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
+                 />
+               </div>
+     
+               <div className="col-span-6 sm:col-span-3">
+                 <label htmlFor="lastname" className="block text-sm font-medium text-gray-700">
+                   Last Name
+                 </label>
+     
+                 <input
+                   onChange={handleChange} required
+                   type="text"
+                   name="lastname"
+                 value={formData.lastname}
+                   className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
+                 />
+               </div>
+               
+               <div className="col-span-6">
+                 <label htmlFor="email" className="block text-sm font-medium text-gray-700"> Email </label>
+     
+                 <input
+                 onChange={handleChange} required
+                   type="email"
+                   name="email"
+                   value={formData.email}
+                   className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
+                 />
+               </div>
+     
+               <div className="col-span-6">
+                 <label htmlFor="password" className="block text-sm font-medium text-gray-700"> Password </label>
+     
+                 <input
+                 onChange={handleChange} required
+                   type="password"
+                   name="password"
+                   value={formData.password}
+                   className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
+                 />
+               </div>
+     
+              
+               <div className="col-span-6">
+                 <label htmlFor="MarketingAccept" className="flex gap-4">
+                   <input
+                     type="checkbox"
+                     id="MarketingAccept"
+                     name="marketing_accept"
+                     className="size-5 rounded-md border-gray-200 bg-white shadow-sm"
+                   />
+     
+                 <span className="text-sm text-gray-700">
+                     I want to receive emails about events, product updates and company announcements.
+                   </span>
+                 </label>
+               </div>
+     
+               <div className="col-span-6">
+                 <p className="text-sm text-gray-500">
+                   By creating an account, you agree to our
+                   <a href="#" className="text-gray-700 underline"> terms and conditions </a>
+                   and
+                   <a href="#" className="text-gray-700 underline"> privacy policy</a>.
+                 </p>
+               </div>
+     
+               <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
+                 <Button type="submit">Create an account</Button>
+                 
+     
+                 <p className="mt-4 text-sm text-gray-500 sm:mt-0">
+                   Already have an account?
+                   <a href="SignIn" className="text-gray-700 underline"> Log in</a>.
+                 </p>
+               </div>
+              
+            </form>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
